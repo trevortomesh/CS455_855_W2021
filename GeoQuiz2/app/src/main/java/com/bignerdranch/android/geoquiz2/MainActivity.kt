ec9202b513d9d6a6e11d21b17f5fa51d86abd2eb
trevor.tomesh <trevor.tomesh@gmail.com>
2021-01-26 19:58:52 -0600

    added GeoQuiz2

---
+++ GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.geoquiz2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+    }
+}
