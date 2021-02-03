dffc287cef24907ca2dbeddcf523b6a34b3be469
trevor.tomesh <trevor.tomesh@gmail.com>
2021-02-03 10:26:22 -0600

    update from feb 3

---
+++ GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz2/Question.kt
@@ -0,0 +1,5 @@
+package com.bignerdranch.android.geoquiz2
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean )
--- hello.kt
+++ hello.kt
@@ -74,8 +74,7 @@ fun main(){
 	val s2 = "${s1.replace("is", "was")}, but now is $foo"
     println(s2)
     
-    */
-    
+  
     val numbers = mutableListOf("une", "deux", "trois", "quatre")
     println(numbers)
     numbers.add("five")
@@ -94,6 +93,129 @@ fun main(){
     println(people == people2)
     println()
     
+    val morenumbers = mutableListOf(1,2,3,4)
+    morenumbers.add(5)
+    morenumbers.removeAt(1)
+    morenumbers[0] = 0
+    morenumbers.shuffle()
+    println(morenumbers)
+    
+    
+    val numberSet = setOf(1,2,3,4)
+    println("Number of elements: ${numberSet.size}")
+    if (numberSet.contains(1)) println("1 is in the set")
+    
+    val numbersBackwards = setOf(4,3,2,1)
+    println("The sets are equal: ${numberSet == numbersBackwards}")
+    
+    
+    
+    //  maps are basically dictionaries
+    //  
+    //  
+
+
+	val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key4" to 4)
+    println("All keys: ${numbersMap.keys}")
+    println("All values: ${numbersMap.values}")
+
+    if("key2" in numbersMap) println("Value by key \"key2\": ${numbersMap["key2"]}")
+    val anotherMap = mapOf("key2" to 2, "key3" to 3, "key4" to 4, "key1" to 1)
+    
+    println(numbersMap == anotherMap)
+    
+    if(1 in numbersMap.values) println("The value 1 is in the map!")
+    if(numbersMap.containsValue(1)) println("The value 1 is STILL in the map!")
+    
+    // mutable maps have write operations
+    
+    val mnumbersMap = mutableMapOf("one" to 1, "two" to 2)
+    mnumbersMap.put("three",3)
+    mnumbersMap["one"] = 11
+    println(mnumbersMap)
+    
+    
+    
+    val a: Int = 1
+	val b = 2
+    println(sum(a,b))
+    println(prod(a,b))
+    printSum(a,b)
+    
+    incramentX()
+    println(x)
+    
+    println(maxOf(a,b))
+    */
+    
+    
+    var fruits = listOf("apple","banana","pineapple")
+    
+    for(item in fruits){
+        println(item)
+    }
+    
+    for(index in fruits.indices){
+        println("item at $index is ${fruits[index]}")
+    }
+    
+    var items = listOf("foo", "bar", "foobar")
+    var index = 0
+    
+    while(index < items.size){
+        println("item at $index is ${items[index]}")
+        index++
+    }
+  
+  //ranges 
+  
+
+	var x = 11
+    var y = 9
+    
+    if(x in 1..y+1){
+        println("fits in range")
+    }
+   
+   
+   for(x in 1..5){
+       println(x)
+   }
+   
+   for(x in 0..100 step 10){
+       println(x)
+   }
+   
+   for(x in 9 downTo 0 step 3){
+       println(x)
+   }
     
 }
 
+
+fun sum(a: Int, b: Int): Int{
+    return a + b
+}
+
+fun prod(a: Int, b: Int) = a*b
+
+fun printSum(a: Int, b: Int): Unit{
+    println("sum of $a and $b is ${a+b}")
+}
+
+fun incramentX(){
+    x+=1
+}
+
+/*
+fun maxOf(a: Int, b: Int): Int{
+    if(a>b){
+        return a
+    }
+    else{
+        return b
+    }
+}*/
+
+
+fun maxOf(a: Int, b: Int) = if(a>b) a else b
