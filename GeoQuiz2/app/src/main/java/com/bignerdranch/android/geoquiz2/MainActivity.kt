2e2bf6eaa9167d54315cc7179289fea923c37f2e
trevor.tomesh <trevor.tomesh@gmail.com>
2021-02-10 10:24:08 -0600

    added activity lifecycle callback overrides

--- GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
+++ GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
@@ -2,10 +2,13 @@ package com.bignerdranch.android.geoquiz2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -52,6 +56,35 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
 
     }
+
+    override fun onStart(){
+       super.onStart()
+        Log.d(TAG,"onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+
+
 
     private fun updateQuestion(){
         val questionTextResId = questionsBank[currentIndex].textResId
