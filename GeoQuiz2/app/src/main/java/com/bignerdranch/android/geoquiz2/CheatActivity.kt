0617a4a17dc06dda8e030d34440e5bf66c9c3ae0
trevor.tomesh <trevor.tomesh@gmail.com>
2021-03-03 11:05:30 -0600

    adding new view

---
+++ GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz2/CheatActivity.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.geoquiz2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+    }
+}
--- GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
+++ GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
@@ -94,6 +94,7 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun updateQuestion(){
+//        Log.d(TAG, "*****Updating question text******", Exception())
         val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
