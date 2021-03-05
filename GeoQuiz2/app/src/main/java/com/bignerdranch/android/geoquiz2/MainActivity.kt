b07d64ac36072b8a5243121d205afecc4a0ef885
trevor.tomesh <trevor.tomesh@gmail.com>
2021-03-05 10:33:09 -0600

    added new activity and intent

--- GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
+++ GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.geoquiz2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var cheatButton: Button
     private lateinit var questionTextView: TextView
 
 
@@ -38,6 +40,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
         
 
@@ -54,6 +57,13 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
 
         }
+
+        cheatButton.setOnClickListener {
+            //start cheat activity...
+            val intent = Intent(this, CheatActivity::class.java)
+            startActivity(intent)
+        }
+
         updateQuestion()
 
     }
