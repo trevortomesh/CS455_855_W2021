be28fe0d688bf83104384060dde8b26ee42ba807
trevor.tomesh <trevor.tomesh@gmail.com>
2021-03-08 10:24:22 -0600

    passing extra with intents

--- GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
+++ GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
@@ -60,7 +60,8 @@ class MainActivity : AppCompatActivity() {
 
         cheatButton.setOnClickListener {
             //start cheat activity...
-            val intent = Intent(this, CheatActivity::class.java)
+           val answerIsTrue = quizViewModel.currentQuestionAnswer
+           val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
             startActivity(intent)
         }
 
