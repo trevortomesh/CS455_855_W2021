39560f30fab7b8415e26187dba5dfb101245ea0d
trevortomesh <slugzzz@gmail.com>
2021-04-09 13:44:20 -0600

    added icon

--- criminalIntent2/app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeListFragment.kt
+++ criminalIntent2/app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -63,7 +64,7 @@ class CrimeListFragment : Fragment() {
         private lateinit var crime: Crime
         private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
         private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
-
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
         init{
             itemView.setOnClickListener(this)
         }
@@ -72,6 +73,11 @@ class CrimeListFragment : Fragment() {
             this.crime = crime
             titleTextView.text = this.crime.title
             dateTextView.text = this.crime.date.toString()
+            solvedImageView.visibility = if(crime.isSolved) {
+                View.VISIBLE
+            }else{
+                View.GONE
+            }
         }
 
         override fun onClick(v: View?) {
